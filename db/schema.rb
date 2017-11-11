b5d6a29c89c1e28b4c810130d1dba743d2466bcc
Vanini Lima <vaninilima@gmail.com>
2017-11-11 15:09:58 -0200

    Inserção de comentários e mudanças no design com Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111115814) do
+ActiveRecord::Schema.define(version: 20171111142911) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
